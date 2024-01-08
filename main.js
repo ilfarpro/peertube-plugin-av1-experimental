28f1188a2d4524d307352726ab50eaf405bddea1
ilfarpro <ilfarpro@protonmail.com>
2024-01-09 02:30:08 +0300

    Force 10bit because Peertube somehow prefers 8 bit -_-

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 async function register ({
   transcodingManager
 }) {
-  transcodingManager.addVODProfile('libsvtav1', 'SVT-AV1 CRF23 Slow', () => ({
+  transcodingManager.addVODProfile('libsvtav1', 'SVT-AV1 CRF23 Preset 7', () => ({
     inputOptions: [],
-    outputOptions: ['-preset', '7', '-crf', '23']
+    outputOptions: ['-pix_fmt yuv420p10le', '-preset', '7', '-crf', '23']
   }));
 
   transcodingManager.addVODEncoderPriority('video', 'libsvtav1', 1000);
 
-  transcodingManager.addVODProfile('libopus', 'SVT-AV1 CRF23 Slow', () => ({
+  transcodingManager.addVODProfile('libopus', 'SVT-AV1 CRF23 Preset 7', () => ({
     inputOptions: [],
     outputOptions: ['-b:a', '320k', '-af', 'loudnorm=I=-14:LRA=11:TP=-1']
   }));
