6f4652e977248cb8aca85837f874e6a5f4c13c83
ilfarpro <ilfarpro@protonmail.com>
2024-01-09 21:19:03 +0300

    Added reasonable presets from 8 to 5, also option without LoudNorm

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 async function register ({
   transcodingManager
 }) {
+
+  transcodingManager.addVODProfile('libsvtav1', 'SVT-AV1 CRF23 Preset 8', () => ({
+    inputOptions: [],
+    outputOptions: ['-pix_fmt yuv420p10le', '-preset', '8', '-crf', '23']
+  }));
+
+  transcodingManager.addVODEncoderPriority('video', 'libsvtav1', 1000);
+
+  transcodingManager.addVODProfile('libopus', 'SVT-AV1 CRF23 Preset 8', () => ({
+    inputOptions: [],
+    outputOptions: ['-b:a', '320k', '-af', 'loudnorm=I=-14:LRA=11:TP=-1']
+  }));
+  
   transcodingManager.addVODProfile('libsvtav1', 'SVT-AV1 CRF23 Preset 7', () => ({
     inputOptions: [],
     outputOptions: ['-pix_fmt yuv420p10le', '-preset', '7', '-crf', '23']
@@ -11,6 +24,78 @@ async function register ({
   transcodingManager.addVODProfile('libopus', 'SVT-AV1 CRF23 Preset 7', () => ({
     inputOptions: [],
     outputOptions: ['-b:a', '320k', '-af', 'loudnorm=I=-14:LRA=11:TP=-1']
+  }));
+
+  transcodingManager.addVODProfile('libsvtav1', 'SVT-AV1 CRF23 Preset 6', () => ({
+    inputOptions: [],
+    outputOptions: ['-pix_fmt yuv420p10le', '-preset', '6', '-crf', '23']
+  }));
+
+  transcodingManager.addVODEncoderPriority('video', 'libsvtav1', 1000);
+
+  transcodingManager.addVODProfile('libopus', 'SVT-AV1 CRF23 Preset 6', () => ({
+    inputOptions: [],
+    outputOptions: ['-b:a', '320k', '-af', 'loudnorm=I=-14:LRA=11:TP=-1']
+  }));
+
+  transcodingManager.addVODProfile('libsvtav1', 'SVT-AV1 CRF23 Preset 5', () => ({
+    inputOptions: [],
+    outputOptions: ['-pix_fmt yuv420p10le', '-preset', '5', '-crf', '23']
+  }));
+
+  transcodingManager.addVODEncoderPriority('video', 'libsvtav1', 1000);
+
+  transcodingManager.addVODProfile('libopus', 'SVT-AV1 CRF23 Preset 5', () => ({
+    inputOptions: [],
+    outputOptions: ['-b:a', '320k', '-af', 'loudnorm=I=-14:LRA=11:TP=-1']
+  }));
+
+  transcodingManager.addVODProfile('libsvtav1', 'SVT-AV1 CRF23 Preset 8 NoLoudNorm', () => ({
+    inputOptions: [],
+    outputOptions: ['-pix_fmt yuv420p10le', '-preset', '8', '-crf', '23']
+  }));
+
+  transcodingManager.addVODEncoderPriority('video', 'libsvtav1', 1000);
+
+  transcodingManager.addVODProfile('libopus', 'SVT-AV1 CRF23 Preset 8 NoLoudNorm', () => ({
+    inputOptions: [],
+    outputOptions: ['-b:a', '320k']
+  }));
+  
+  transcodingManager.addVODProfile('libsvtav1', 'SVT-AV1 CRF23 Preset 7 NoLoudNorm', () => ({
+    inputOptions: [],
+    outputOptions: ['-pix_fmt yuv420p10le', '-preset', '7', '-crf', '23']
+  }));
+
+  transcodingManager.addVODEncoderPriority('video', 'libsvtav1', 1000);
+
+  transcodingManager.addVODProfile('libopus', 'SVT-AV1 CRF23 Preset 7 NoLoudNorm', () => ({
+    inputOptions: [],
+    outputOptions: ['-b:a', '320k']
+  }));
+
+  transcodingManager.addVODProfile('libsvtav1', 'SVT-AV1 CRF23 Preset 6 NoLoudNorm', () => ({
+    inputOptions: [],
+    outputOptions: ['-pix_fmt yuv420p10le', '-preset', '6', '-crf', '23']
+  }));
+
+  transcodingManager.addVODEncoderPriority('video', 'libsvtav1', 1000);
+
+  transcodingManager.addVODProfile('libopus', 'SVT-AV1 CRF23 Preset 6 NoLoudNorm', () => ({
+    inputOptions: [],
+    outputOptions: ['-b:a', '320k']
+  }));
+
+  transcodingManager.addVODProfile('libsvtav1', 'SVT-AV1 CRF23 Preset 5 NoLoudNorm', () => ({
+    inputOptions: [],
+    outputOptions: ['-pix_fmt yuv420p10le', '-preset', '5', '-crf', '23']
+  }));
+
+  transcodingManager.addVODEncoderPriority('video', 'libsvtav1', 1000);
+
+  transcodingManager.addVODProfile('libopus', 'SVT-AV1 CRF23 Preset 5 NoLoudNorm', () => ({
+    inputOptions: [],
+    outputOptions: ['-b:a', '320k']
   }));
 
   transcodingManager.addVODEncoderPriority('audio', 'libopus', 1000);
